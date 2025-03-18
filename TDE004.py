04bec1a7fc82948c1519ab968d23fb09a0da2424
eduardo.ellwanger <dudi.cap07@gmail.com>
2025-03-18 12:35:58 -0300

    exercicios 1 ao 4 do TDE de raciocinio algoritimo,
    A erros no exec 4

---
+++ TDE004.py
@@ -0,0 +1,18 @@
+# Faça um algoritmo que recebe o valor de um produto e calcule os seguintes valores:
+# (1) a vista com 5% de desconto; (2) o valor da parcela em 2x; (3) o valor da parcela
+# em 3x com acréscimo de 5%
+
+input('Escolha um produto: uma bolsa, um relógio, um manga de chainsaw man')
+
+produto = (x, y, c)
+
+x = bolsa
+y =  relógio
+c = "manga_csm"
+
+print("Otimo, escolha a forma de pagamento: (1) a vista com 5% de desconto; (2 parcelado em 2x; (3) parcelado em 3x com acréscimo de 5%")
+
+A = " a vista com 5% de desconto"
+B = "parcelado em 2x"
+G = "parcelado em 3x com acréscimo de 5%"
+
